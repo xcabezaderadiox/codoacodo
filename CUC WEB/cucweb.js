f97c7826194fbc1b11ebfe005ee525b4bb8f5638
xcabezaderadiox <christianmontesdeoca89@yahoo.com.ar>
2021-10-25 19:12:44 -0300

    form

--- CUC WEB/cucweb.js
+++ CUC WEB/cucweb.js
@@ -1,11 +1,28 @@
 function validar() {
-  let x = document.forms["form"]["nombre"].value;
+  var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let x = document.forms["form"]["nombre"].value; 
+  let y = document.forms["form"]["email"].value;
+  let z = document.forms["form"]["mensaje"].value;
   if (x == "") {
     alert("Debe ingresar un nombre");
     return false;
   }
-}
+  if (y == "") {
+    alert("Debe ingresar un email valido");
+    return false;
+  }
+  if (!filter.test(y)) 
+  {
+    alert('Debe ingresar un email valido');
+    email.focus;
+    return false;
+  }
+  if (z.length < 5 ){ 
+    alert("Debe ingresar un mensaje con un minimo de 5 caracteres");
+    return false;
+  }
 
+}
 
 
 
